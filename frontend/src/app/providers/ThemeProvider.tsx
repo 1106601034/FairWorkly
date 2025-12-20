cb553996d1f47c0b9bce64e0c5b72819daca5b7c
Lillian Liu <lillianliu2013@gmail.com>
2025-12-21 00:08:12 +1100

    missing {} in the import 'theme', just fix it

--- frontend/src/app/providers/ThemeProvider.tsx
+++ frontend/src/app/providers/ThemeProvider.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import { CssBaseline } from '@mui/material'
-import theme from '@/shared/styles/theme'
+import { theme } from '@/shared/styles/theme'
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
